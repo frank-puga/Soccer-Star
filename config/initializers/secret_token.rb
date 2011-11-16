83e68f42d9483950842e687c1e8fbf5b14f46108
frank-puga <fpuga@cs.utsa.edu>
2011-11-15 22:30:24 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerStar::Application.config.secret_token = 'b3c53c1ecd1179a1fe80f7139afe8143a8fca9eee61fb967be529012ff1f6caf1692361380dc79ecc353550feffaf37dc7118ab2d920f855c71fba26af4ef772'
