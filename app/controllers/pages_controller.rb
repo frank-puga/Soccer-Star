4c6cbb752ce599a4fd71c0222d3102e41560397b
fpuga <fpuga@cs.utsa.edu>
2011-11-17 00:45:38 -0600

    Added all pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def registration
+  end
+
+  def contact
+  end
+
+  def schedule
+  end
+
+  def info
+  end
+
+  def signin
+  end
+
+end
