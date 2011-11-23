1e1d73c1eed36aceca586fd7330b146cb4a00324
fpuga <fpuga@cs.utsa.edu>
2011-11-23 16:13:10 -0600

    start pages branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def registration
+    @title = "Registration"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def schedule
+    @title = "Schedule"
   end
 
   def info
+    @title = "Info"
   end
 
   def signin
+    @title = "Sign In"
   end
 
 end
