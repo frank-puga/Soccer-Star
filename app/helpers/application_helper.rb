3414e0cfe47b7216f0dbd69d021fc4aea7fcc85d
fpuga <fpuga@cs.utsa.edu>
2011-11-24 04:10:26 -0600

    worked on home page, set routes, & added css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def logo
-    image_tag("banner.png", :alt => "Soccer Star", :class => "round")
-  end
 end
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+    it "should have a home page at '/'" do
+      get '/'
+      response.should have_selector("title", :content => "Home")
+    end
+  
+     it "should have an about page at '/about'" do
+      get '/registration'
+      response.should have_selector("title", :content => "Registration")
+    end
+     
+     it "should have a contact page at '/contact'" do
+      get '/contact'
+      response.should have_selector("title", :content => "Contact")
+    end
+    
+    it "should have the right links on the layout" do
+      visit root_path
+      response.should have_selector('title', :content => "Home")
+      click_link "Registration"
+      response.should have_selector('title', :content => "Registration")
+      click_link "Contact"
+      response.should have_selector('title', :content => "Contact")           
+    end
+end
