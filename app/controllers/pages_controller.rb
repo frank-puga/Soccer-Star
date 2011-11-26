ee0bb09f6a4b1f9f613ad0ebf47e8489c6e706ca
fpuga <fpuga@cs.utsa.edu>
2011-11-26 00:56:43 -0600

    worked on contact page & added random pics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,23 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @images  = ["player.png", "player1.png", "player2.png", "player3.png", "player4.png", "player5.png"]
+    @random_no = rand(6)
+    @random_image = @images[@random_no]
   end
 
   def registration
     @title = "Registration"
+    @images  = ["player.png", "player1.png", "player2.png", "player3.png", "player4.png", "player5.png"]
+    @random_no = rand(6)
+    @random_image = @images[@random_no]
   end
 
   def contact
     @title = "Contact"
+    @images  = ["player.png", "player1.png", "player2.png", "player3.png", "player4.png", "player5.png"]
+    @random_no = rand(6)
+    @random_image = @images[@random_no]
   end
 
 end
